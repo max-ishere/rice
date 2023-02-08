b74deeaaab821dae246a89f55a2650278c5f9ac6
max-ishere <max-ishere@github.com>
2023-02-09 01:15:38 +0200

    fix: Moved rice/main.py into tofi.py
    main() in rice root was used to generate the tofi config. With things like blocks you can generate the md file by giving module path to `-m rice.blocks *block*`. You cannot do that with tofi since there are many custom actions. Later when the generation will be standardized main will move to generators/__main__.py and only take module path as arg.

--- generators/tofi.py
+++ generators/tofi.py
@@ -355,3 +355,27 @@ class Tofi(ConfigFile):
             + "\n\n"
             + conf
         )
+
+
+def main():
+    from rice.blocks.tofi_apple_theme import AppleTofi
+
+    fonts = Fonts(regular=Font("Comic Mono"))
+    colors = ColorTheme(
+        text=Color(0xFF, 0xFF, 0xFF),
+        primary=Color(0x55, 0x33, 0xEE),
+        on_primary=Color(0x33, 0x22, 0xEE),
+        primary_dark=Color(0x11, 0x08, 0x55),
+        on_primary_dark=Color(0xFF, 0xFF, 0xFF),
+        secondary=Color(0xEE, 0x33, 0xEE),
+        secondary_dark=Color(0x55, 0x33, 0x55),
+        background=Color(0x11, 0x11, 0x11),
+        foreground=Color(0x99, 0x99, 0x99),
+    )
+
+    theme = Theme(font=fonts, font_size=FontSize(normal=11), colors=colors)
+    AppleTofi(theme).default_header().deploy()
+
+
+if __name__ == "__main__":
+    main()
